b3782771b4d93b05cc0a4a5abe26fe1f63b89354
cortadocodes <cortado.codes@protonmail.com>
2021-09-08 12:06:04 +0100

    ENH: Add 10s delay between start and finish log statements

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
+import time
+
+
 def run(analysis):
     analysis.logger.info("Started example analysis.")
+    time.sleep(10)
     analysis.output_values = [1, 2, 3, 4, 5]
     analysis.logger.info("Finished example analysis.")
