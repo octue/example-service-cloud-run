7c6b9f2a3f5838e9665623496ad5dcc99a629ee6
cortadocodes <cortado.codes@protonmail.com>
2021-11-17 17:46:57 +0000

    ENH: Reduce sleep in app

--- app.py
+++ app.py
@@ -3,6 +3,6 @@ import time
 
 def run(analysis):
     analysis.logger.info("Started example analysis.")
-    time.sleep(100)
+    time.sleep(5)
     analysis.output_values = [1, 2, 3, 4, 5]
     analysis.logger.info("Finished example analysis.")
