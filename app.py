2d8ede5821fb6dd72e37fb088fd362a2af99201f
cortadocodes <cortado.codes@protonmail.com>
2022-01-11 17:40:20 +0000

    ENH: Add example package and submodule

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import time
 
+from example_package.submodule import do_something
+
 
 def run(analysis):
     analysis.logger.info("Started example analysis.")
+    do_something()
     time.sleep(5)
     analysis.output_values = [1, 2, 3, 4, 5]
     analysis.logger.info("Finished example analysis.")
