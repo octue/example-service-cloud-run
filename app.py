59e52418d3f25a9832cd53c6649ccae6d0a0d126
cortadocodes <cortado.codes@protonmail.com>
2022-04-20 12:28:38 +0100

    ENH: Reduce sleep time in app

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 def run(analysis):
     logger.info("Started example analysis.")
     do_something()
-    time.sleep(5)
+    time.sleep(2)
     analysis.output_values = [1, 2, 3, 4, 5]
 
     with tempfile.TemporaryDirectory() as temporary_directory:
