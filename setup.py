171be15e1577ec1e38d02f54aa4f3d4a59e9b025
cortadocodes <cortado.codes@protonmail.com>
2022-08-31 10:44:39 +0100

    DEP: Update to latest octue

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name="example-service",
-    version="0.1.8",
+    version="0.1.9",
     install_requires=[
-        "octue==0.29.8",
+        "octue @ https://github.com/octue/octue-sdk-python/archive/feature/add-heartbeat-messages-to-children.zip",
     ],
     url="https://www.github.com/octue/example-service-cloud-run",
     author="cortadocodes",
