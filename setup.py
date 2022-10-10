bc2186899317adcafb2a7c07e5d3723217d22dee
cortadocodes <cortado.codes@protonmail.com>
2022-10-10 16:59:48 +0100

    OPS: Add a namespace to the service
    BREAKING CHANGE: Use the new service ID to ask the service questions: `octue/example-service-cloud-run`

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name="example-service",
-    version="0.1.9",
+    version="0.2.0",
     install_requires=[
-        "octue==0.38.0",
+        "octue @ https://github.com/octue/octue-sdk-python/archive/feature/add-topic-and-subscription-creation-command.zip",
     ],
     url="https://www.github.com/octue/example-service-cloud-run",
     author="cortadocodes",
