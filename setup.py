c7f89a8d9073e3ae8f111bdf12924656cbfc38bd
cortadocodes <cortado.codes@protonmail.com>
2022-12-21 12:37:45 +0000

    DEP: Use latest `octue` version

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name="example-service",
     version="0.2.0",
     install_requires=[
-        "octue @ https://github.com/octue/octue-sdk-python/archive/feature/add-topic-and-subscription-creation-command.zip",
+        "octue @ https://github.com/octue/octue-sdk-python/archive/devops/improve-deployment-dockerfile.zip",
     ],
     url="https://www.github.com/octue/example-service-cloud-run",
-    author="cortadocodes",
-    author_email="cortado.codes@protonmail.com",
+    author="Marcus Lugg",
+    author_email="marcus@octue.com",
     description="An example Octue service for deploying to Google Cloud Run.",
 )
