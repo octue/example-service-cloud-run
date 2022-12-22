bc50b9d22e74c5a0fddc3000b44072ad6bb7627e
cortadocodes <cortado.codes@protonmail.com>
2022-12-22 15:10:49 +0000

    DEP: Move `pre-commit` to dev dependencies

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     version="0.2.0",
     install_requires=[
         "octue==0.43.2",
-        "pre-commit>=2.20.0",
     ],
     url="https://www.github.com/octue/example-service-cloud-run",
     author="Marcus Lugg",
