2bedfa83b412b8d16af3cd62e86644b96b26475e
cortadocodes <cortado.codes@protonmail.com>
2023-03-23 14:43:42 +0000

    TST: Update test bucket name and add missing output location

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,7 +9,7 @@ from octue.resources import Manifest
 
 REPOSITORY_ROOT = os.path.dirname(os.path.dirname(__file__))
 TWINE_PATH = os.path.join(REPOSITORY_ROOT, "twine.json")
-TEST_BUCKET_NAME = "octue-test-bucket"
+TEST_BUCKET_NAME = "octue-sdk-python-test-bucket"
 
 
 class TestApp(unittest.TestCase):
@@ -24,6 +24,9 @@ class TestApp(unittest.TestCase):
         runner = Runner(
             app_src=os.path.join(REPOSITORY_ROOT, "example_service_cloud_run"),
             twine=TWINE_PATH,
+            # Output location is taken from the app configuration automatically in a service but not if `Runner` is used
+            # by itself.
+            output_location="gs://octue-sdk-python-test-bucket/example_output_datasets",
         )
 
         with patch("google.cloud.storage.blob.Blob.generate_signed_url", mock_generate_signed_url):
